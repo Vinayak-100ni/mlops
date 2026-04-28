b79405857d07d433e52b0b43931527057bc35f94
Vinayak soni <112276881+Vinayak-100ni@users.noreply.github.com>
2026-04-28 14:53:08 +0530

    Update api.py

--- mlflow/project/api.py
+++ mlflow/project/api.py
@@ -9,28 +9,75 @@ model = joblib.load("data/model/churn_model.pkl")
 # Create FastAPI app
 app = FastAPI()
 
-# Input schema
+# -----------------------------
+# Input Schema
+# -----------------------------
 class CustomerData(BaseModel):
-    gender: int
+    gender: str
     SeniorCitizen: int
-    Partner: int
-    Dependents: int
+    Partner: str
+    Dependents: str
     tenure: int
-    PhoneService: int
+    PhoneService: str
+    MultipleLines: str
+    InternetService: str
+    OnlineSecurity: str
+    OnlineBackup: str
+    DeviceProtection: str
+    TechSupport: str
+    StreamingTV: str
+    StreamingMovies: str
+    Contract: str
+    PaperlessBilling: str
+    PaymentMethod: str
     MonthlyCharges: float
     TotalCharges: float
 
-# Health check
+
+# -----------------------------
+# Health Check
+# -----------------------------
 @app.get("/")
 def home():
     return {"message": "Churn Prediction API Running"}
 
-# Prediction endpoint
+
+# -----------------------------
+# Prediction Endpoint
+# -----------------------------
 @app.post("/predict")
 def predict(data: CustomerData):
 
+    # Convert input to DataFrame
     input_df = pd.DataFrame([data.dict()])
 
+    # -----------------------------
+    # Encode categorical columns
+    # -----------------------------
+    categorical_columns = [
+        "gender",
+        "Partner",
+        "Dependents",
+        "PhoneService",
+        "MultipleLines",
+        "InternetService",
+        "OnlineSecurity",
+        "OnlineBackup",
+        "DeviceProtection",
+        "TechSupport",
+        "StreamingTV",
+        "StreamingMovies",
+        "Contract",
+        "PaperlessBilling",
+        "PaymentMethod"
+    ]
+
+    for col in categorical_columns:
+        input_df[col] = input_df[col].astype("category").cat.codes
+
+    # -----------------------------
+    # Prediction
+    # -----------------------------
     prediction = model.predict(input_df)
 
     result = int(prediction[0])
